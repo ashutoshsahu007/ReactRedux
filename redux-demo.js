f1bbe93a848626c0f83efe6e711a81346b276194
ashutosh sahu <ppaskumar@gmail.com>
2025-08-28 15:20:27 +0530

    2 more action type added

--- redux-demo.js
+++ redux-demo.js
@@ -3,6 +3,8 @@ const redux = require("redux");
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") return { counter: state.counter + 1 };
   if (action.type === "decrement") return { counter: state.counter - 1 };
+  if (action.type === "incrementby2") return { counter: state.counter + 2 };
+  if (action.type === "decrementby2") return { counter: state.counter - 2 };
   return state;
 };
 
@@ -15,5 +17,5 @@ const counterSubscriber = () => {
 
 store.subscribe(counterSubscriber);
 
-store.dispatch({ type: "increment" });
-store.dispatch({ type: "decrement" });
+store.dispatch({ type: "incrementby2" });
+store.dispatch({ type: "decrementby2" });
